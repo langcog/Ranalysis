7f91f1d5d5f9f8a5bfd06127a9da6918a70bf649
Molly Lewis <mll@stanford.edu>
2014-02-04 17:01:33 -0800

    added subject anonymize function

--- useful.R
+++ useful.R
@@ -110,3 +110,14 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     }
   }
 }
+
+# annonymize subject ids by giving them a value 1:num_subjects
+anonymize.sids <- function(df, subject_column_label) {
+  subj_col = which(names(df) == subject_column_label)
+  temp <- data.frame(workerid = unique(df[,subj_col]))
+  temp$subid <- 1:length(unique(df[,subj_col]))
+  index <- match(df[,subj_col], temp$workerid)
+  df$subids <- temp$subid[index]
+  df[,subj_col] <- NULL
+  return(df)
+}
