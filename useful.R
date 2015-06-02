f84201333149d8876b22ad349ed7126bc582fd67
Michael C. Frank <mcfrank@stanford.edu>
2015-06-02 14:16:38 -0700

    updates from lab meeting

--- useful.R
+++ useful.R
@@ -1,34 +1,27 @@
-library(ggplot2)
-library(reshape2)
-library(bootstrap)
-library(lme4)
-library(stringr)
-library(lubridate)
-library(plyr) # first load plyr then dplyr from https://github.com/hadley/dplyr
 library(dplyr)
+library(ggplot2)
+library(bootstrap)
 
 ## add some style elements for ggplot2
 theme_set(theme_bw())
 
-## standard error of the mean
-sem <- function (x) {
-  sd(x,na.rm=TRUE) / sqrt(length(x))
-}
+# @aen - luminance palette
+# @mikabr - solarized?
 
 ## NA functions
-na.mean <- function(x) {mean(x,na.rm=T)}
-na.median <- function(x) {median(x,na.rm=T)}
-na.sum <- function(x) {sum(x,na.rm=T)}
-na.sd <- function(x) {sd(x,na.rm=T)}
+na.mean <- function(x) {mean(x, na.rm=T)}
+na.median <- function(x) {median(x, na.rm=T)}
+na.sum <- function(x) {sum(x, na.rm=T)}
+na.sd <- function(x) {sd(x, na.rm=T)}
+
+## standard error of the mean
+sem <- function (x) {
+  na.sd(x) / sqrt(length(x))
+}
 
 ## convert to number
 to.n <- function(x) {
   as.numeric(as.character(x))
-}
-
-## inverse logistic
-inv.logit <- function (x) {
-  exp(x) / (1 + exp(x)) 
 }
 
 ## number of unique subs
@@ -46,7 +39,8 @@ ci.high <- function(x,na.rm=T) {
 ## get stars for significance testing
 getstars <- function(x) {
   if (x > .1) {return("")}
-  if (x < .001) {return("***")}
-  if (x < .01) {return("**")}
+  if (x < .1 & x >= .05) {return(".")}
   if (x < .05) {return("*")}
+  if (x < .01) {return("**")}
+  if (x < .001) {return("***")}
 }
